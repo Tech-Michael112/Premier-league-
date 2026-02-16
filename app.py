6cab92eb4c883fc31542be6773421c894c5a673e
Michael Covenant <129428958+Tech-Michael112@users.noreply.github.com>
2026-02-16 06:59:59 +0100

    Change template from index_new.html to index.html

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def parse_file(file):
 
 @app.route('/')
 def index():
-    return render_template('index_new.html')
+    return render_template('index.html')
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
